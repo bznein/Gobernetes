a2f02d581c0aff4749d640e11b948d501d3da214
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2020-08-04 21:11:08 +0100

    A lot of improvement and new features

--- cmd/main.go
+++ cmd/main.go
@@ -6,12 +6,26 @@ import (
 	"os"
 	"time"
 
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 
 	tm "github.com/buger/goterm"
+	kb "github.com/eiannone/keyboard"
+)
+
+type KeyboardInput int
+
+const (
+	Pod       KeyboardInput = 0
+	Sts       KeyboardInput = 1
+	ArrowUp   KeyboardInput = 2
+	ArrowDown KeyboardInput = 3
+	Delete    KeyboardInput = 4
+	Quit      KeyboardInput = -1
 )
 
 func main() {
@@ -24,23 +38,64 @@ func main() {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %+v", err)
 	}
-	inputChan := make(chan int)
+	inputChan := make(chan KeyboardInput)
 	closeChan := make(chan bool)
-	go showData(config, clientset, 0, closeChan)
+	go showData(config, clientset, 0, 0, closeChan)
 	go waitForInput(inputChan)
 	val := 0
+	line := 0
 	for {
 		select {
-		case val = <-inputChan:
+		case modifier := <-inputChan:
 			closeChan <- true
-			fmt.Fprintf(os.Stderr, "Changing format")
-			go showData(config, clientset, val, closeChan)
+			switch modifier {
+			case Pod, Sts:
+				val = int(modifier)
+				line = 0
+			case ArrowUp:
+				line--
+			case ArrowDown:
+				line++
+			case Delete:
+				deleteResource(clientset, line, val)
+				line = 0
+			case Quit:
+				return
+			}
+			go showData(config, clientset, val, line, closeChan)
 			go waitForInput(inputChan)
 		}
 	}
 }
 
-func showData(config *rest.Config, clientset *kubernetes.Clientset, what int, closeChan chan bool) {
+func listStsNamespaced(clientset *kubernetes.Clientset, namespace string) []appsv1.StatefulSet {
+	sts, err := clientset.AppsV1().StatefulSets(namespace).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %+v", err)
+	}
+	return sts.Items
+}
+
+func listPodNamespaced(clientset *kubernetes.Clientset, namespace string) []corev1.Pod {
+	pods, err := clientset.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %+v", err)
+	}
+	return pods.Items
+}
+
+func deleteResource(clientset *kubernetes.Clientset, line int, val int) {
+	switch KeyboardInput(val) {
+	case Pod:
+		pod := listPodNamespaced(clientset, "nikolas")[line]
+		clientset.CoreV1().Pods(pod.Namespace).Delete(context.TODO(), pod.Name, metav1.DeleteOptions{})
+	case Sts:
+		sts := listStsNamespaced(clientset, "nikolas")[line]
+		clientset.AppsV1().StatefulSets(sts.Namespace).Delete(context.TODO(), sts.Name, metav1.DeleteOptions{})
+	}
+}
+
+func showData(config *rest.Config, clientset *kubernetes.Clientset, what int, line int, closeChan chan bool) {
 
 	for {
 		select {
@@ -49,56 +104,88 @@ func showData(config *rest.Config, clientset *kubernetes.Clientset, what int, cl
 		default:
 			switch what {
 			case 0:
-				readAndPrintPods(config, clientset)
+				readAndPrintPods(config, clientset, line)
 			case 1:
-				readAndPrintSts(config, clientset)
+				readAndPrintSts(config, clientset, line)
 			}
 		}
 	}
 }
 
-func readAndPrintSts(config *rest.Config, clientset *kubernetes.Clientset) {
-
+func readAndPrintSts(config *rest.Config, clientset *kubernetes.Clientset, line int) {
+	sts := listStsNamespaced(clientset, "nikolas")
 	tm.Clear()
-	sts, err := clientset.AppsV1().StatefulSets("nikolas").List(context.TODO(), v1.ListOptions{})
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %+v", err)
-	}
 	tm.MoveCursor(1, 1)
-
-	for _, ss := range sts.Items {
-		tm.Printf("Statefulset: %+v\t%s\n\n", ss.Name, ss.Kind)
+	if line < 0 {
+		line = 0
+	}
+	if line >= len(sts) {
+		line = len(sts) - 1
+	}
+	for i, ss := range sts {
+		if i == line {
+			tm.Printf(tm.Background(tm.Color(fmt.Sprintf("Statefulset: %+v\t%s\n\n", ss.Name, ss.Kind), tm.RED), tm.GREEN))
+			tm.Println()
+		} else {
+			tm.Printf("Statefulset: %+v\t%s\n\n", ss.Name, ss.Kind)
+		}
 	}
 
 	tm.Flush() // Call it every time at the end of rendering
 
-	time.Sleep(time.Second)
+	time.Sleep(time.Millisecond * 200)
 
 }
 
-func readAndPrintPods(config *rest.Config, clientset *kubernetes.Clientset) {
-
+func readAndPrintPods(config *rest.Config, clientset *kubernetes.Clientset, line int) {
+	pods := listPodNamespaced(clientset, "nikolas")
 	tm.Clear()
-	pods, err := clientset.CoreV1().Pods("nikolas").List(context.TODO(), v1.ListOptions{})
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %+v", err)
-	}
 	tm.MoveCursor(1, 1)
-
-	tm.Printf("There are %d pods in the cluster\n", len(pods.Items))
-	for _, pod := range pods.Items {
-		tm.Printf("Pod: %+v\t%s\n\n", pod.Name, pod.Status.Phase)
+	if line < 0 {
+		line = 0
+	}
+	if line >= len(pods) {
+		line = len(pods) - 1
+	}
+	tm.Printf("There are %d pods in the cluster\n", len(pods))
+	for i, pod := range pods {
+		if i == line {
+			tm.Printf(tm.Background(tm.Color(fmt.Sprintf("Pod: %+v\t%s\n\n", pod.Name, pod.Status.Phase), tm.RED), tm.GREEN))
+			tm.Println()
+		} else {
+			tm.Printf("Pod: %+v\t%s\n\n", pod.Name, pod.Status.Phase)
+		}
 	}
 
 	tm.Flush() // Call it every time at the end of rendering
 
-	time.Sleep(time.Second)
+	time.Sleep(time.Millisecond * 200)
 
 }
 
-func waitForInput(inputChan chan int) {
-	var i int
-	_, _ = fmt.Scanf("%d", &i)
-	inputChan <- i
+func waitForInput(inputChan chan KeyboardInput) {
+	char, key, err := kb.GetSingleKey()
+	if err != nil {
+		panic(err)
+	}
+	if char != 0 {
+		switch char {
+		case '0':
+			inputChan <- Pod
+		case '1':
+			inputChan <- Sts
+		case 'q', 'Q':
+			inputChan <- Quit
+		case 'd', 'D':
+			inputChan <- Delete
+		}
+	} else {
+		switch key {
+		case kb.KeyArrowUp:
+			inputChan <- ArrowUp
+		case kb.KeyArrowDown:
+			inputChan <- ArrowDown
+		}
+	}
 
 }
--- go.mod
+++ go.mod
@@ -4,11 +4,11 @@ go 1.14
 
 require (
 	github.com/buger/goterm v0.0.0-20200322175922-2f3e71b85129
+	github.com/eiannone/keyboard v0.0.0-20200508000154-caf4b762e807
 	github.com/imdario/mergo v0.3.10 // indirect
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
 	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
-	k8s.io/api v0.18.6 // indirect
+	k8s.io/api v0.18.6
 	k8s.io/apimachinery v0.18.6
 	k8s.io/client-go v0.18.6
-	k8s.io/utils v0.0.0-20200731180307-f00132d28269 // indirect
 )
